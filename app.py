7ae1a46592c979d0f347c8a43a1d5fc661217e68
KirilPolitov <polito.kiril@gmail.com>
2026-02-16 12:23:29 +0200

    Implement Book Checker App with Streamlit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+books = [
+  "Joe Mama",
+  "Mein Kampf",
+  "To catch a predator"
+  "Tequitla"]
+st.title("Book Checker App")
+st.write("enter a book title to check if it exists in the database.")
+userInput = st.text_input("Book Title")
+if st.button("Check Book"):
+  if userInput.strip() == "":
+    st.warning("Please enter title")
+  elif userInput in books:
+    st.success("The book exists")
+  else:
+    st.error("THE BOOK DOESNT EXIST IDIOT!!!")
