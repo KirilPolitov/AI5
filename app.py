84a2398ddb0e60504ab6f5a1d2b6c3a0dfa0c94c
KirilPolitov <polito.kiril@gmail.com>
2026-02-23 12:33:03 +0200

    Modify book addition functionality in Library App
    Updated the app to allow adding books with title, author, and price.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
 books = [
-  "Joe Mama",
-  "Mein Kampf",
-  "To catch a predator"
-  "Tequitla"]
-st.title("Book Checker App")
+  #"Joe Mama",
+  #"Mein Kampf",
+  #"To catch a predator"
+  #"Tequitla"
+  
+]
+st.title("Library App")
 st.write("enter a book title to check if it exists in the database.")
 userInput = st.text_input("Book Title")
 if st.button("Check Book"):
@@ -15,5 +17,15 @@ if st.button("Check Book"):
     st.success("The book exists")
   else:
     st.error("THE BOOK DOESNT EXIST IDIOT!!!")
-    newBook = st.text_input("Add book")
-    books.append(newBook)
+    title = st.text_input("Enter title")
+    author = st.text_input("Enter author")
+    price = st.number_input("Enter price")
+    if st.button("Confirm"):
+      book = {
+        "title": title,
+        "author": author,
+        "price": price
+      }
+      st.session_state.books.append(book)
+      st.success("Added")
+      
