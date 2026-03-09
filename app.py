9d17ad037731564bee4eda195e8476683dde4132
KirilPolitov <polito.kiril@gmail.com>
2026-03-09 08:49:24 +0200

    sosumi

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-import streamlit as st
-
-books = [
-  #"Joe Mama",
-  #"Mein Kampf",
-  #"To catch a predator"
-  #"Tequitla"
-  
-]
-st.title("Library App")
-st.write("enter a book title to check if it exists in the database.")
-userInput = st.text_input("Book Title")
-if st.button("Check Book"):
-  if userInput.strip() == "":
-    st.warning("Please enter title")
-  elif userInput in books:
-    st.success("The book exists")
-  else:
-    st.error("THE BOOK DOESNT EXIST IDIOT!!!")
-    title = st.text_input("Enter title")
-    author = st.text_input("Enter author")
-    price = st.number_input("Enter price")
-    if st.button("Confirm"):
-      book = {
-        "title": title,
-        "author": author,
-        "price": price
-      }
-      st.session_state.books.append(book)
-      st.success("Added")
-      
+import streamlit as st st.title("Приложение")
+# Създаваме масив (списък), ако още не #съществува if "books" not in st.session_state:
+st.session_state.books = []
+#
+#+ Добавяне на книга
+#
+st.header("+ Добави книга") 
+title = st.text_input("3aглавие") 
+author = st.text_input("ABTOP")
+price = st.number_input("Цена", min_value=0.0) 
+if st.button("Добави книгата"):
+   book = { "title": title, "author": author, "price": price
+}
+st.session_state.books.append(book) st.success("Книгата е добавена!")
+#
+# Покажи всички книги
+#
+if st.button(" Покажи всички книги"):
+   if len(st.session_state.books) 0:        st.write("Няма добавени книги.")
+   else:
+      for book in st.session_state.books:                  st.write("зaглавие:", bооk["title"])                   st.write("Aвтоp:", book["author"])                st.write("цeна:", book["price"])              st.write("-----")
+#
+#Търсене по автор
+st.header(" Търсене по автор")
+search_author = st.text_input("Въведи име Hа автор") 
+if st.button("Търси по автор"):
+   found = False
+   for book in st.session_state.books: 
+      if book["author"] == search_author: 
+         st.write(book)
+         found = True
+      if found == False:
+         st.write("Няма намерени книги от този автор.")
